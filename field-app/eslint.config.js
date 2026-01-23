2a85d7e65fd4c36251792e0003376ee1cfec6e1f
Tyler Krueger <kruegety@oregonstate.edu>
2026-01-23 16:12:12 -0500

    establish ci scripts and checks for eslint and prettier move index to tabs directory

--- field-app/eslint.config.js
+++ field-app/eslint.config.js
@@ -1,10 +1,16 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const prettier = require('eslint-config-prettier/flat');
 
 module.exports = defineConfig([
   expoConfig,
+  prettier,
   {
-    ignores: ['dist/*'],
+    ignores: [
+    'dist',
+    'node_modules',
+    'build'
+    ],
   },
 ]);
