48504cd2554b5ccbb46917e158db9e02b03f14e2
Tyler Krueger <kruegety@oregonstate.edu>
2026-01-24 09:04:39 -0500

    prettier updates

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,12 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
-const prettier = require('eslint-config-prettier/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const prettier = require("eslint-config-prettier/flat");
 
 module.exports = defineConfig([
   expoConfig,
   prettier,
   {
-    ignores: [
-    'dist',
-    'node_modules',
-    'build'
-    ],
+    ignores: ["dist", "node_modules", "build"],
   },
 ]);
