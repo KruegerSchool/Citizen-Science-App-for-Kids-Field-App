6583ef126dd23f424426309c343177ef62967646
Tyler Krueger <kruegety@oregonstate.edu>
2026-02-11 15:05:14 -0500

    utility function to generate student_id for app use

--- utility_functions/student_id_gen.ts
+++ utility_functions/student_id_gen.ts
@@ -2,11 +2,10 @@
 // storing in local storage via watermelondb as this is not a user generated
 // value. reference: https://watermelondb.dev/docs/Advanced/LocalStorage
 import uuid from 'react-native-uuid';
-import database from '../database/database';
 
 export default async function generateStudentID() {
   const studentID = uuid.v4();
   // store student ID locally for use with observations
-  await database.localStorage.set('studentID', studentID);
+  // TBD
   return studentID;
 }
