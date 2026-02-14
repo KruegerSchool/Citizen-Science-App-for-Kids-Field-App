824ce395d341a275b9b566b0c53069de43e88c05
Tyler Krueger <kruegety@oregonstate.edu>
2026-02-13 23:50:21 -0500

    more and better fixes

--- utility_functions/network_status.ts
+++ utility_functions/network_status.ts
@@ -1,6 +1,4 @@
 // code for determining and parsing network status for a device
-import useConnectionStatus from "../app/stores/network";
-
 export default function connectionStatusText(isConnected: boolean | null) {
   if (isConnected === null) {
     return "Checking network connection...";
